0aabf93c06d55fe76f5e6117555b3ca812884576
Abdulrahim Mansour <amansour@Abdulrahims-MacBook-Air.local>
2022-04-05 17:36:24 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,40 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { Avatar, Container, Grid, Typography } from '@mui/material'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Abdulrahim Mansour</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header/>
+        <Container>
+          <Grid container rowSpacing={2} direction="column" alignItems="center" justifyContent="flexStart">
+            <Grid item>
+              <Avatar
+                alt="Abdulrahim Mansour"
+                src="/static/images/portrait.jpg"
+                sx={{ mt:"10px", width: 250, height: 250 }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant='h4'>Abdulrahim Mansour</Typography>
+            </Grid>
+            <Grid item>
+              <Typography 
+                variant='p'
+                sx={{fontSize: ""}}
+              >
+                Full-Stack Software Developer
+              </Typography>
+            </Grid>
+          </Grid>
+        </Container>
       </main>
 
       <Footer />
