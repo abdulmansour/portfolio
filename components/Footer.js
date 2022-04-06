3042c58b6e3cd627abd0318fa2a1d3b4d0f1a2e1
Abdulrahim Mansour <amansour@Abdulrahims-MacBook-Air.local>
2022-04-05 23:11:32 -0400

    added resume, about me section and polished css

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,19 @@
+import { Link } from '@mui/material'
 import styles from './Footer.module.css'
 
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+
 export default function Footer() {
+  const iconCss = {color:"primary", marginX: "5px", '&:hover': { backgroundColor: "grey.300", cursor: "pointer"}, display: "flex"};
   return (
     <footer className={styles.footer}>
-      mansour.abdulrahim@gmail.com
+      <div>
+        <Link href="https://github.com/abdulmansour"><GitHubIcon sx={iconCss}/></Link>
+        <Link href="https://www.linkedin.com/in/mansourabdulrahim/"><LinkedInIcon sx={iconCss}/></Link>
+        <Link href="mailto:mansour.abdulrahim@gmail.com"><EmailIcon sx={iconCss}/></Link>
+      </div>
     </footer>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -18,6 +18,15 @@ import EmailIcon from '@mui/icons-material/Email';
 const pages = ['Projects'];
 
 const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
     <AppBar position="static">
@@ -39,28 +48,41 @@ const ResponsiveAppBar = () => {
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
+              onClick={handleOpenNavMenu}
               color="inherit"
             >
               <MenuIcon />
             </IconButton>
             <Menu
               id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
               keepMounted
               transformOrigin={{
                 vertical: 'top',
                 horizontal: 'left',
               }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
               sx={{
                 display: { xs: 'block', md: 'none' },
               }}
             >
               <MenuItem>
-                <Link href="https://github.com/abdulmansour"><GitHubIcon sx={{marginX: "5px", '&:hover': { backgroundColor: "primary.light"}, display: { xs: 'flex', md: 'none' }}}/></Link>
-                <Link href="https://www.linkedin.com/in/mansourabdulrahim/"><LinkedInIcon sx={{marginX: "5px",'&:hover': { backgroundColor: "primary.light"}, display: { xs: 'flex', md: 'none' }}}/></Link>
-                <Link href="mailto:mansour.abdulrahim@gmail.com"><EmailIcon sx={{marginX: "5px",'&:hover': { backgroundColor: "primary.light"}, display: { xs: 'flex', md: 'none' }}}/></Link>
+                <Link href="https://github.com/abdulmansour"><GitHubIcon sx={{marginX: "5px", '&:hover': { backgroundColor: "grey.300"}, display: { xs: 'flex', md: 'none' }}}/></Link>
+                <Link href="https://www.linkedin.com/in/mansourabdulrahim/"><LinkedInIcon sx={{marginX: "5px",'&:hover': { backgroundColor: "grey.300"}, display: { xs: 'flex', md: 'none' }}}/></Link>
+                <Link href="mailto:mansour.abdulrahim@gmail.com"><EmailIcon sx={{marginX: "5px",'&:hover': { backgroundColor: "grey.300"}, display: { xs: 'flex', md: 'none' }}}/></Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href='static/pdf/cv.pdf' download>
+                <Typography textAlign="center">Resume</Typography>
+                </Link>
               </MenuItem>
               {pages.map((page) => (
-                <MenuItem key={page}>
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
                     <Link href={`/${page.toLowerCase()}`}>
                       <Typography textAlign="center">{page}</Typography>
                     </Link>
@@ -79,9 +101,17 @@ const ResponsiveAppBar = () => {
             </Typography>
           </Link>
           <Box sx={{ flexGrow: 1, justifyContent: "right", display: { xs: 'none', md: 'flex' } }}>
+            <Link href='static/pdf/cv.pdf' download>
+              <Button
+                sx={{ my: 2, color: 'white', display: 'block', '&:hover': { backgroundColor: "primary.light"} }}
+              >
+                Resume
+              </Button>
+            </Link>
             {pages.map((page) => (
               <Link key={page} href={`/${page.toLowerCase()}`}>
                 <Button
+                  onClick={handleCloseNavMenu}
                   sx={{ my: 2, color: 'white', display: 'block', '&:hover': { backgroundColor: "primary.light"} }}
                 >
                   {page}
@@ -90,7 +120,7 @@ const ResponsiveAppBar = () => {
             ))}
           </Box>
           <Box sx={{ justifyContent: "right", display: 'flex' }}>
-            <Link href="https://github.com/abdulmansour"><GitHubIcon sx={{marginX: "5px", '&:hover': { backgroundColor: "primary.light"}, display: { xs: 'none', md: 'flex' }}}/></Link>
+            <Link href="https://github.com/abdulmansour"><GitHubIcon sx={{marginX: "5px", '&:hover': { backgroundColor: "grey.light"}, display: { xs: 'none', md: 'flex' }}}/></Link>
             <Link href="https://www.linkedin.com/in/mansourabdulrahim/"><LinkedInIcon sx={{marginX: "5px",'&:hover': { backgroundColor: "primary.light"}, display: { xs: 'none', md: 'flex' }}}/></Link>
             <Link href="mailto:mansour.abdulrahim@gmail.com"><EmailIcon sx={{marginX: "5px",'&:hover': { backgroundColor: "primary.light"}, display: { xs: 'none', md: 'flex' }}}/></Link>
           </Box>
